cae53be34b03286c51ad6cb766aaa7c84de61c71
Osman Turan <osmanturancom@gmail.com>
2019-01-02 01:09:51 +0300

    Migrate to Hugo

--- dev-server.js
+++ dev-server.js
@@ -1,15 +1,27 @@
+const path = require("path");
+const fs = require("fs");
 const connect = require("connect");
+const compression = require("compression");
 const serveStatic = require("serve-static");
 
+const serverPort = 5000;
+const wwwRoot = path.join(__dirname, "bin");
+
 const app = connect();
+
 app.use((req, res, next) => {
-    console.log(req.url);
-    if (/\.wasm$/i.test(req.url)) {
-        res.setHeader("Content-Type", "application/wasm");
-    }
-    next();
+  console.log(req.url);
+
+  // Set WebAssembly modules content type.
+  if (/\.wasm$/i.test(req.url)) {
+    res.setHeader("Content-Type", "application/wasm");
+  }
+
+  next();
 });
-app.use(serveStatic("./bin"));
-app.listen(5000, () => {
-    console.log("HTTP server started.");
+
+app.use(compression());
+app.use(serveStatic(wwwRoot));
+app.listen(serverPort, () => {
+  console.log(`HTTP server started at port ${serverPort}.`);
 });
